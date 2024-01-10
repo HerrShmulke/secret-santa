541dec0115626bbac8431230299248466bd71df4
Aleksey <shmulter@mail.ru>
2024-01-11 01:10:56 +0700

    Add fastify to inversify

--- src/configurations/inversify.ts
+++ src/configurations/inversify.ts
@@ -1,4 +1,8 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
+import { GLOBAL_TYPES } from '../constants/global-types';
+import { fastifyInstance } from './fastify';
 
 export const containter = new Container();
+
+containter.bind(GLOBAL_TYPES.FASTIFY).toConstantValue(fastifyInstance);
